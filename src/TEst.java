95261a3bd34fea3f8c0bcb0084f072e8b0da7e99
ad <ferrariflag1@abv.bg>
2018-07-01 13:26:48 +0300

    just test second
    yes

--- src/TEst.java
+++ src/TEst.java
@@ -3,7 +3,7 @@ public class TEst {
 
 	public static void main(String[] args) {
 		System.out.println("Hellooo");
-		System.out.println("How are you ");
+		System.out.println("I hope you are OK ! ");
 
 	}
 
